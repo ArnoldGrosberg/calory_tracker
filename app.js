d75b467141775c07d9f4500c3427314296ed3a60
Arnold Grosberg <arnold.grosberg@khk.ee>
2021-12-06 19:55:59 +0200

    Created Controllers and Data Structure

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+// Storage Controller
+// create later
 
+// Item Controller
+const ItemCtrl = (function(){
+	// Item Constructor
+	const Item = function(id, name, calories){
+		this.id = id
+		this.name = name
+		this.calories = calories
+	}
+	const data = {
+		items: [
+		{id: 0, name: 'Steak Dinner', calories: 1200},
+		{id: 0, name: 'Cookie', calories: 400},
+		{id: 0, name: 'Eggs', calories: 300}
+		],
+		total: 0
+	}
+	return {logData: function(){
+		return data
+	}}
+})();
+
+// UI Controller
+const UICtrl = (function(){
+	
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl){
+	return {
+		init: function(){
+			console.log('Initializing App')
+		}
+	}
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init()
